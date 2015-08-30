fd25259d109becfc524448cd8a7f09eebf7a3221
Carlos Rabelo <developer@carlosrabelo.com.br>
2015-08-30 09:25:01 -0400

    BoltCircleCalculator

---
+++ BoltCircleCalculator/config.lua
@@ -0,0 +1,162 @@
+-- config.lua for project: Trades Math Calculator
+-- Managed with http://OutlawGameTools.com
+-- Copyright 2013 . All Rights Reserved.
+-- cpmgen config.lua
+
+
+if string.sub(system.getInfo("model"),1,4) == "iPad" then
+    application = 
+    {
+        content =
+        {
+            width = 360,
+            height = 480,
+            scale = "letterbox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        SpriteHelperSettings = 
+		{
+			imagesSubfolder = "Images",
+		},
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            }
+        }
+    }
+
+elseif string.sub(system.getInfo("model"),1,2) == "iP" and display.pixelHeight > 960 then
+    application = 
+    {
+        content =
+        {
+            width = 320,
+            height = 568,
+            scale = "letterbox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        SpriteHelperSettings = 
+		{
+			imagesSubfolder = "Images",
+		},
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            }
+        }
+    }
+
+elseif string.sub(system.getInfo("model"),1,2) == "iP" then
+    application = 
+    {
+        content =
+        {
+            width = 320,
+            height = 480,
+            scale = "letterbox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        SpriteHelperSettings = 
+		{
+			imagesSubfolder = "Images",
+		},
+        notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            }
+        }
+    }
+elseif display.pixelHeight / display.pixelWidth > 1.72 then
+    application = 
+    {
+        content =
+        {
+            width = 320,
+            height = 570,
+            scale = "letterbox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+        SpriteHelperSettings = 
+        {
+          imagesSubfolder = "Images",
+        },
+        license =
+        {
+          google =
+          {
+            key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA32oOvCKT0RdawkN+UnbjvBX+vOHvXjVnUshZKd+MyL8icuwl2HBG0x7v8sbC5iNGlEgdvAVuVD4E5N0OxHWTb14itA8R6Q0e0Oau586ieJAR1mNXNWC10S0ipokK3n64MdPaXB1S8oaOqEqn87rmODHd5UkiM1uQy+0OFxUhwN5pfbiWaQ5ej+Hzi3FFFXLkXt2jme8/1B20ZIL13HUzT1lZzorqy7n6Qnt18iK/4mttQCZQFJcsXP0b2YlJJoc+oPqB6ODiv2qb6SRjs+v4X69SHsr2331YZTaba15q3tUMQRgGjWQcdc0UyFN/jjcm7B4UfiPFppj6pD8rZCWOLQIDAQAB",
+            policy = "serverManaged",
+          },
+        },
+    }
+else
+    application = 
+    {
+      content =
+        {
+            width = 320,
+            height = 512,
+            scale = "letterbox",
+            xAlign = "center",
+            yAlign = "center",
+            imageSuffix = 
+            {
+                ["@2x"] = 1.5,
+                ["@4x"] = 3.0,
+            },
+        },
+      SpriteHelperSettings = 
+        {
+          imagesSubfolder = "Images",
+        },
+      notification = 
+        {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                    }
+                }
+        },
+      license =
+        {
+          google =
+            {
+              key =  "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA32oOvCKT0RdawkN+UnbjvBX+vOHvXjVnUshZKd+MyL8icuwl2HBG0x7v8sbC5iNGlEgdvAVuVD4E5N0OxHWTb14itA8R6Q0e0Oau586ieJAR1mNXNWC10S0ipokK3n64MdPaXB1S8oaOqEqn87rmODHd5UkiM1uQy+0OFxUhwN5pfbiWaQ5ej+Hzi3FFFXLkXt2jme8/1B20ZIL13HUzT1lZzorqy7n6Qnt18iK/4mttQCZQFJcsXP0b2YlJJoc+oPqB6ODiv2qb6SRjs+v4X69SHsr2331YZTaba15q3tUMQRgGjWQcdc0UyFN/jjcm7B4UfiPFppj6pD8rZCWOLQIDAQAB",
+              policy = "serverManaged",
+            },
+        },
+  }
+end
---
+++ BoltCircleCalculator/main.lua
@@ -0,0 +1,82 @@
+-- Project: Trades Math Calculator
+-- Description: Mobile version of Harry Powell's Trades Math Calculator
+-- Developed By: Stephen Powell
+-- Corona Version: 1.0
+-- Managed with http://OutlawGameTools.com
+--
+-- Copyright 2013 . All Rights Reserved.
+---- cpmgen main.lua
+
+-- cpmgen main.lua
+local physicalW = math.round( (display.contentWidth  - display.screenOriginX*2) / display.contentScaleX)
+local physicalH = math.round( (display.contentHeight - display.screenOriginY*2) / display.contentScaleY)
+
+--Require
+local loadsave = require("loadsave")
+local device = require("device")
+local licensing = require( "licensing" )
+local composer = require( "composer" )
+local analytics = require( "analytics" )
+
+if not device.isApple then
+
+  analytics.init( "2YBNRT7WPG6X7VF7XZDV" )
+
+licensing.init( "google" )
+
+local function alertListener ( event )
+  if "clicked" == event.action then
+
+    local i = event.index    
+    if i == 1 then
+      native.requestExit()
+    end        
+  end
+end
+
+local function licensingListener( event )
+   local verified = event.isVerified
+   if not event.isVerified then
+      --failed verify app from the play store, we print a message
+      native.showAlert ( "Could Not Authorize", "There was a problem contacting the Google licensing servers. Please check internet connection and try again.", { "Close" }, alertListener)
+    else
+      composer.gotoScene( "menu")
+   end
+end
+
+if device.isApple then
+  analytics.init( "MSHRNG36QRQM5WT97TQV" )
+end
+
+licensing.verify( licensingListener )
+end
+
+local timesOpen2 = loadsave.loadTable("timesOpen2.json")
+--timesOpen2.opened = 4
+
+if (menuOpened == nil) then
+    menuOpened = {}
+    menuOpened.opened = false
+    loadsave.saveTable(menuOpened, "menuOpen.json")
+end
+  
+  if (timesOpen2 == nil) then
+    timesOpen2 = {}
+    timesOpen2.opened = 0
+    loadsave.saveTable(timesOpen2, "timesOpen2.json")
+  elseif timesOpen2.opened ~= "never" then
+    --timesOpen2.opened = 0
+    if timesOpen2.opened < 7 then
+      timesOpen2.opened = timesOpen2.opened + 1
+      loadsave.saveTable(timesOpen2, "timesOpen2.json")
+    end
+  end
+ 
+print(system.getInfo("model") .. " " .. system.getInfo("platformVersion"))
+if device.isApple then
+  composer.gotoScene( "menu")
+end
+
+
+
+
