1081a5371b32ba43555a0e1506e5463e54326708
Carlos Rabelo <developer@carlosrabelo.com.br>
2015-09-01 07:47:12 -0400

    PlanetDefender

---
+++ PlanetDefender/spriteanims.lua
@@ -0,0 +1,13 @@
+local spriteSequences = {}
+
+-- START:sequenceData
+spriteSequences.spaceshipSequence = {
+    { 
+        name = SPR_SHIP,
+        start = spritedata:getFrameIndex( SPR_SHIP ),
+        count = 1
+    }
+}
+-- END:sequenceData
+
+return spriteSequences
