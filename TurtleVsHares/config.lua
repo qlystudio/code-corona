87ae7084714bce0b61393b6936f0d3aa6b88fb86
Carlos Rabelo <developer@carlosrabelo.com.br>
2015-09-01 10:00:04 -0400

    TurtleVsHares

---
+++ TurtleVsHares/config.lua
@@ -0,0 +1,17 @@
+application =
+{
+        content =
+        {
+				graphicsCompatibility = 1,
+        		width = 320,
+                height = 480,
+                scale = "Letterbox",
+                fps = 30,
+                antialias = true,
+                
+                imageSuffix =
+				{
+					["@2x"] = 2,
+				},
+        },
+}
